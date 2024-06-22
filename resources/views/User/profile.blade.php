7f3a68f5ed585e2462ef70b929e8d775295876d8
Glen Brian G. Decado <glen.decado@gmail.com>
2024-06-23 06:32:52 +0800

    user -profile

---
+++ resources/views/User/profile.blade.php
@@ -0,0 +1,8 @@
+<div>
+    id: {{Auth::User()->id}}<br>
+    img: {{Auth::User()->img}}<br>
+    role: {{Auth::User()->role}}<br>
+    name: {{Auth::User()->name}}<br>
+    email: {{Auth::User()->email}}<br>
+    password: {{Auth::User()->password}}<br>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Livewire\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', User::class);
+Route::view('/profile', 'User.profile');
