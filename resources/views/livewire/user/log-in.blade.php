eee1b89569606372dbea04a4315f174409df2242
Glen Brian G. Decado <glen.decado@gmail.com>
2024-06-28 03:48:40 +0800

    post method login

--- resources/views/livewire/user/log-in.blade.php
+++ resources/views/livewire/user/log-in.blade.php
@@ -1,6 +1,6 @@
 <div>
     <center>
-        <form wire:submit.prevent="login">
+        <form wire:submit.prevent="login" method="post">
             <input type="email" wire:model="email">
             <input type="password" wire:model="password">
             <button type="submit">submit</button>
