b6ec44d6dd619a8659dd92e6cdd0d89e514a8b7b
Glen Brian G. Decado <glen.decado@gmail.com>
2024-07-03 03:54:39 +0800

    request -delete

--- resources/views/request/delete-request.blade.php
+++ resources/views/request/delete-request.blade.php
@@ -1,4 +1,2 @@
-@php
-$id = $req['id'];
-@endphp
-<button @click="if (confirm('Are you sure you want to delete this user?')) $dispatch('request-delete', { id: '{{$id}}' })">Delete</button>
+
+<button @click="if (confirm('Are you sure you want to delete this user?')) $dispatch('request-delete', { id: '{{$req->id}}' })">Delete</button>
