2c3111e671d153eba7ef27267eea61da860a2df7
Glen Brian G. Decado <glen.decado@gmail.com>
2024-07-02 23:25:07 +0800

    request -add request on modal

--- resources/views/livewire/request/request.blade.php
+++ resources/views/livewire/request/request.blade.php
@@ -1,31 +1,5 @@
 <div>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>Request ID</th>
-                <th>Faculty ID</th>
-                <th>Category</th>
-                <th>Concerns</th>
-                <th>Date</th>
-                <th>Time</th>
-                <th>Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($request as $req)
-            <tr>
-                <td>{{$req['id']}}</td>
-                <td>{{$req['faculty_id']}}</td>
-                <td>{{$req['category']}}</td>
-                <td>{{$req['concerns']}}</td>
-                <td>{{date_format($req['created_at'], "Y/m/d")}}</td>
-                <td>{{date_format($req['created_at'], "g:i a")}}</td>
-                <td>{{$req['status']}}</td>
-                <td>@include('request.delete-request')</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-
     @include('request.add-request')
+
+    @include('request.table-request')
 </div>
--- resources/views/request/add-request.blade.php
+++ resources/views/request/add-request.blade.php
@@ -1,17 +1,26 @@
-<div>
-    <form wire:submit.prevent="addRequest">
-        <div>
-            <label for="category">Category</label>
-            <input type="text" id="category" wire:model="category">
-            @error('category') <span class="error">{{ $message }}</span> @enderror
-        </div>
+{{--button to open modal--}}
 
-        <div>
-            <label for="concerns">Concern</label>
-            <textarea id="concerns" wire:model="concerns"></textarea>
-            @error('concerns') <span class="error">{{ $message }}</span> @enderror
-        </div>
+<button type="button" @click="$dispatch('open-modal',  'example-modal')">Add request </button>
 
-        <button @click="$dispatch('update-request')" type="submit">Submit</button>
-    </form>
-</div>
+{{--modal--}}
+<x-modal name="example-modal">
+
+    <div>
+        <form wire:submit.prevent="addRequest">
+            <div>
+                <label for="category">Category</label>
+                <input type="text" id="category" wire:model="category">
+                @error('category') <span class="error">{{ $message }}</span> @enderror
+            </div>
+
+            <div>
+                <label for="concerns">Concern</label>
+                <textarea id="concerns" wire:model="concerns"></textarea>
+                @error('concerns') <span class="error">{{ $message }}</span> @enderror
+            </div>
+
+            <button @click="$dispatch('update-request')" type="submit">Submit</button>
+        </form>
+    </div>
+
+</x-modal>
