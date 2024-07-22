9e9ee14042a6915aaa436535c7b970357e718f39
Glen Brian G. Decado <glen.decado@gmail.com>
2024-07-23 04:32:42 +0800

    simple navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,20 +4,20 @@ $route = Request::route()->getName() ?? '/';
 
 
 <nav class="bg-blue-950 text-white flex flex-row gap-2">
-    <a href="/" wire:navigate class="flex items-center justify-center h-[50px] px-[20px] hover:bg-yellow-400 hover:text-blue-950 {{$route == '/' ? 'bg-yellow-400 text-blue-950' : '' }}">
+    <a href="/" wire:navigate class="flex items-center justify-center h-[56px] px-[20px] hover:bg-yellow-400 hover:text-blue-950 {{$route == '/' ? 'bg-yellow-400 text-blue-950' : '' }}">
         Home
     </a>
 
-    <a href="{{route('profile')}}" wire:navigate class="flex items-center justify-center h-[50px] px-[20px]  hover:bg-yellow-400 hover:text-blue-950 {{$route == 'profile' ? 'bg-yellow-400 text-blue-950' : '' }}">
+    <a href="{{route('profile')}}" wire:navigate class="flex items-center justify-center h-[56px] px-[20px]  hover:bg-yellow-400 hover:text-blue-950 {{$route == 'profile' ? 'bg-yellow-400 text-blue-950' : '' }}">
         Profile
     </a>
 
-    <a href="{{route('request')}}" wire:navigate class="flex items-center justify-center h-[50px] px-[20px]  hover:bg-yellow-400 hover:text-blue-950 {{$route == 'request' ? 'bg-yellow-400 text-blue-950' : '' }}">
+    <a href="{{route('request')}}" wire:navigate class="flex items-center justify-center h-[56px] px-[20px]  hover:bg-yellow-400 hover:text-blue-950 {{$route == 'request' ? 'bg-yellow-400 text-blue-950' : '' }}">
         Request
     </a>
 
     @if (Auth::user()->role == 'Mis Staff')
-    <a href="{{route('manage-user')}}" wire:navigate class="flex items-center justify-center h-[50px] px-[20px]  hover:bg-yellow-400 hover:text-blue-950 {{$route == 'manage-user' ? 'bg-yellow-400 text-blue-950' : '' }}">
+    <a href="{{route('manage-user')}}" wire:navigate class="flex items-center justify-center h-[56px] px-[20px]  hover:bg-yellow-400 hover:text-blue-950 {{$route == 'manage-user' ? 'bg-yellow-400 text-blue-950' : '' }}">
         Users
     </a>
     @endif
