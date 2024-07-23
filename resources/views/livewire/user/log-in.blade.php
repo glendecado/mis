bd567ebb34dd7d98dbb30144adec8ed5c2c817c1
Glen Brian G. Decado <glen.decado@gmail.com>
2024-07-24 07:43:44 +0800

    login -frontend -showing the errors on login

--- resources/views/livewire/user/log-in.blade.php
+++ resources/views/livewire/user/log-in.blade.php
@@ -14,14 +14,14 @@
                         <input type="email" name="email" id="email" placeholder="Email Address" class="peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none" autocomplete="NA" wire:model.blur="email" />
                         <label for="email" class="pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800">Email Address</label>
                         @if ($emailError)
-                        <div class="mt-1 text-sm text-red-600 absolute">❌ {{ $emailError }}</div>
+                        <div class="mt-1 text-xs text-red-600 absolute">❌ {{ $emailError }}</div>
                         @endif
                     </div>
                     <div class="relative mt-9">
                         <input type="password" name="password" id="password" placeholder="Password" class="peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none" wire:model.blur="password" />
                         <label for="password" class="pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800">Password</label>
                         @if ($passwordError)
-                        <div class="mt-1 text-sm text-red-600 absolute">❌ {{ $passwordError }}</div>
+                        <div class="mt-1 text-xs text-red-600 absolute">❌ {{ $passwordError }}</div>
                         @endif
                     </div>
 
