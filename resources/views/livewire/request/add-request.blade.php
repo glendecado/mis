0978ceb3bfb8ff9267db56787921b94f58c61d74
Glen Brian G. Decado <glen.decado@gmail.com>
2024-08-06 02:28:23 +0800

    request -fix -add image

--- resources/views/livewire/request/add-request.blade.php
+++ resources/views/livewire/request/add-request.blade.php
@@ -24,7 +24,7 @@
                 <div class="mb-5">
                     <div x-data="{ category: @entangle('category'), others: @entangle('others')};">
                         <label for="role" class="block mb-2 text-sm font-medium text-gray-900">Category</label>
-                        <select id="s" x-model="category" x-show="category == 'Computer/laptop/printer' && " class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mb-5">
+                        <select id="s" x-model="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mb-5">
                             <option value="Computer/laptop/printer">Computer/laptop/printer</option>
                             <option value="Network">Network</option>
                             <option value="Software">Software</option>
--- resources/views/livewire/request/view-request.blade.php
+++ resources/views/livewire/request/view-request.blade.php
@@ -5,37 +5,45 @@
     @livewire('request.add-request')
     @endif
 
-
-
     <div class="overflow-hidden">
-
-
         @foreach ($request as $req)
-        <div class="border bg-blue-950 gap-4 grid grid-rows-4 text-white p-4 m-4 rounded-md md:grid-rows-1 md:grid-cols-4 ">
+        <div class=" bg-blue-950 mx-10 md:mx-[10%] gap-4 grid grid-rows-4 text-white p-4 m-4 rounded-lg md:grid-rows-1 md:grid-cols-4 overflow-auto">
 
-            <div class="text-center flex flex-col">from: {{$req->faculty->user->name}}
+
+            <div class="ml-20 flex flex-col">
+                @if (Auth::user()->role == 'Mis Staff')
+                from: {{$req->faculty->user->name}}
+
+                <img src="{{asset('storage/'. $req->faculty->user->img)}}" alt="profile" class="rounded-[100%] w-[100px] h-[100px] object-center object-cover">
+
+                @endif
                 <span wire:key="{{$req->id }}" class="mt-4">Request ID: {{$req['id']}}</span>
             </div>
-
-            <div class="w-[100%]">Category: {{$req['category']}}<br><textarea disabled class="p-2 resize-none w-full rounded-md overflow-auto">{{$req['concerns']}}</textarea></div>
-
+            <div class="w-[100%]">
+                Category: <span class="bg-blue-100 p-1 rounded-md text-blue-950">{{$req['category']}}</span><br>
+                <span class="text-sm">concerns</span>
+                <textarea disabled class="text-blue-950 p-2 resize-none w-full h-40 rounded-md overflow-auto">{{$req['concerns']}}</textarea>
+            </div>
             <div class="flex flex-row items-center gap-2">
                 <div>date:
                     <span class="bg-blue-100 p-1 rounded-md text-blue-950">
                         {{date_format($req['created_at'], "Y/m/d")}}
                     </span>
                 </div>
-
                 <div>time:
                     <span class="bg-blue-100 p-1 rounded-md text-blue-950">
-                        {{date_format($req['created_at'], "g:ia")}}</span>
+                        {{date_format($req['created_at'], "g:ia")}}
+                    </span>
                 </div>
-                <div>Status: {{$req['status']}}</div>
+                <div>Status:
+                    <span class="bg-blue-100 p-1 rounded-md text-blue-950">
+                        {{$req['status']}}
+                    </span>
+                </div>
             </div>
-
             <div class="text-center">
                 {{--view--}}
-                <button type="button" @click="$dispatch('open-modal',  'view-request-{{$req->id}}');">View </button>
+                <button type="button" @click="$dispatch('open-modal',  'view-request-{{$req->id}}');">View</button>
                 <x-modal name="view-request-{{$req->id}}">
                     <div class="flex flex-row text-blue-950">
                         <div wire:key="{{$req->id }}">
@@ -73,16 +81,14 @@
                         </div>
                         <div>
                             <button @click="$dispatch('view-assigned', {id: {{$req->id }}})">Assign Technical Staff</button>
-
                         </div>
                     </div>
-
                 </x-modal>
-
             </div>
         </div>
-
         @endforeach
 
         @livewire('task.view-task')
     </div>
+
+</div>
