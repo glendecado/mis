996eeb6c826ffead7a062c6cfbb0843acd22430c
Glen Brian G. Decado <glen.decado@gmail.com>
2024-08-06 02:29:03 +0800

    migrations -add default on image

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->enum('role',['Mis Staff','Technical Staff', 'Faculty']);
-            $table->string('img')->nullable();
+            $table->string('img')->default('profile_images/default/default.png');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
