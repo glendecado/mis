eb88fe2ddf96478682b111672f08a7dff486d789
Glen Brian G. Decado <glen.decado@gmail.com>
2024-08-07 04:40:30 +0800

    delete route chache

--- routes/web.php
+++ routes/web.php
@@ -34,16 +34,3 @@ Route::middleware(['Mis'])->group(function () {
 
 
 
-// routes/web.php
-
-Route::delete('/deletenotif/{id}', function ($id) {
-    $notifications = Cache::get('notif-' . Auth::id());
-
-    // Check if notifications exist and remove the specified one
-    if (isset($notifications[$id])) {
-        unset($notifications[$id]);
-        Cache::put('notif-' . Auth::id(), array_values($notifications)); // Reindex array
-    }
-
-    return redirect('/');
-});
