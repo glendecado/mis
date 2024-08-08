7d89af6003b768d912be577a1d268fd5109fcc7f
Glen Brian G. Decado <glen.decado@gmail.com>
2024-08-08 10:44:07 +0800

    modal edit

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -46,7 +46,7 @@ $maxWidth = [
     <div x-show="show" class="fixed inset-0 transform transition-all" x-on:click="show = false" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
         <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
     </div>
-    <div class="w-[100%] h-[100vh] flex items-center justify-center">
+    <div class="w-[100%] h-[100vh] mt-[50px]">
         <!-- card -->
         <div x-show="show" class="mb-6 bg-white p-[10px] pt-[50px] rounded-lg overflow-auto shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
             <!-- Close button -->
