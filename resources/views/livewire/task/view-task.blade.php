9f198e018fde8c5b4b9c5a1f52af7379a3724574
Glen Brian G. Decado <glen.decado@gmail.com>
2024-09-02 06:02:01 +0800

    -opti

--- resources/views/livewire/task/view-task.blade.php
+++ resources/views/livewire/task/view-task.blade.php
@@ -1,8 +1,11 @@
 <div>
     @livewire('task.add-task')
-    @foreach($request as $req)
-    <x-modal name="assigned-{{$req->id ?? ''}}">
+
+    <x-modal name="assigned">
+        @if(isset($id))
         @switch(Auth::user()->role)
+
+        {{--IF THE USER IS MIS STAFF--}}
         @case('Mis Staff')
 
         <table>
@@ -25,23 +28,24 @@
 
                         {{--numbers of pending according to assigned technical staff--}}
                         {{
-                            DB::table('requests')
-                            ->join('tasks', 'requests.id', '=', 'tasks.request_id')
-                            ->where('tasks.technicalStaff_id', $tech->user->id)
-                            ->where('requests.status', 'pending')
-                            ->count()
-                        }}
+                                DB::table('requests')
+                                ->join('tasks', 'requests.id', '=', 'tasks.request_id')
+                                ->where('tasks.technicalStaff_id', $tech->user->id)
+                                ->where('requests.status', 'pending')
+                                ->count()
+                            }}
                     </td>
                     <td></td>
                     <td>
                         @if (in_array($tech->user->id, $task))
-                        <button class="bg-blue-900 text-white" @click="$dispatch('add-task', {request_id: '{{$req->id ?? ''}}', tech_id: '{{$tech->user->id}}'}); $dispatch('update-task'); $dispatch('update-request')">add</button>
+                        <button class="bg-blue-900 text-white" @click="$dispatch('add-task', {request_id: '{{$id ?? ''}}', tech_id: '{{$tech->user->id}}'}); $dispatch('update-task'); $dispatch('update-request')">add</button>
                         @else
-                        <button class="bg-slate-200" @click="$dispatch('add-task', {request_id: '{{$req->id ?? ''}}', tech_id: '{{$tech->user->id}}'}) ; $dispatch('update-task');$dispatch('update-request')">add</button>
+                        <button class="bg-slate-200" @click="$dispatch('add-task', {request_id: '{{$id ?? ''}}', tech_id: '{{$tech->user->id}}'}) ; $dispatch('update-task');$dispatch('update-request')">add</button>
                         @endif
 
                     </td>
                 </tr>
+
                 @endforeach
             </tbody>
         </table>
@@ -49,23 +53,28 @@
 
         @break
 
+        {{--IF THE USER IS FACULTY--}}
         @case('Faculty')
 
         @foreach ($technicalStaff as $tech)
+
         @if (in_array($tech->user->id, $task))
         {{$tech->user->name}}
         @endif
+
         @endforeach
 
         @break
 
+
+        {{--IF THE USER IS Technical Staff--}}
         @case('Technical Staff')
 
         @break
 
 
         @endswitch
-
+        @endif
     </x-modal>
-    @endforeach
+
 </div>
