605bda82beb89c1d009457c4e9aa5382ce139d8e
Glen Brian G. Decado <glen.decado@gmail.com>
2024-09-03 10:00:37 +0800

    navbar -change color

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -11,7 +11,7 @@
 
                 @foreach ($user as $u)
                 <a href=" /profile/{{$u->id}}" class="p-2 flex flex-row w-52 hover:bg-slate-500">
-                    <img src="{{ asset('storage/' . $u->img) }}" alt="" class="w-12 rounded-[100%]"> 
+                    <img src="{{ asset('storage/' . $u->img) }}" alt="" class="w-12 rounded-[100%]">
                     {{$u->name}}
                 </a>
                 @endforeach
@@ -24,18 +24,18 @@
         </div>
 
     </div>
-    <nav class="bg-blue-950 text-white flex flex-row gap-2 overflow-auto">
+    <nav class="bg-yellow-400 text-blue-950 flex flex-row  overflow-auto">
 
-        <a href="/" wire:navigate class="flex items-center justify-center h-[56px] px-[20px] hover:bg-yellow-400 hover:text-blue-950 {{$route == '/' ? 'bg-yellow-400 text-blue-950' : '' }}">
+        <a href="/" wire:navigate class="flex items-center justify-center h-[56px] px-[20px] hover:text-yellow-400 hover:bg-blue-950 {{$route == '/' ? 'text-yellow-400 bg-blue-950' : '' }}">
             Home
         </a>
 
-        <a href="{{ route('profile', ['user' => Auth::id()]) }}" wire:navigate class="flex items-center justify-center h-[56px] px-[20px]  hover:bg-yellow-400 hover:text-blue-950 {{$route == 'profile' ? 'bg-yellow-400 text-blue-950' : '' }}">
+        <a href="{{ route('profile', ['user' => Auth::id()]) }}" wire:navigate class="flex items-center justify-center h-[56px] px-[20px]  hover:text-yellow-400 hover:bg-blue-950 {{$route == 'profile' ? 'text-yellow-400 bg-blue-950' : '' }}">
             Profile
         </a>
 
         <div class="h-fit w-fit">
-            <a href="{{route('request')}}" wire:navigate class="flex items-center justify-center h-[56px] px-[20px]  hover:bg-yellow-400 hover:text-blue-950 {{$route == 'request' ? 'bg-yellow-400 text-blue-950' : '' }}">
+            <a href="{{route('request')}}" wire:navigate class="flex items-center justify-center h-[56px] px-[20px]  hover:text-yellow-400 hover:bg-blue-950 {{$route == 'request' ? 'text-yellow-400 bg-blue-950' : '' }}">
                 Request
                 @if (Auth::user()->role == 'Mis Staff')
                 <span class="relative left-0 bottom-2 text-md text-red-900" id="notif">
@@ -49,7 +49,7 @@
         </div>
 
         @if (Auth::user()->role == 'Mis Staff')
-        <a href="{{route('manage-user')}}" wire:navigate class="flex items-center justify-center h-[56px] px-[20px]  hover:bg-yellow-400 hover:text-blue-950 {{$route == 'manage-user' ? 'bg-yellow-400 text-blue-950' : '' }}">
+        <a href=" {{route('manage-user')}}" wire:navigate class="flex items-center justify-center h-[56px] px-[20px]  hover:text-yellow-400 hover:bg-blue-950    {{$route == 'manage-user' ? 'text-yellow-400 bg-blue-950' : ''  }}">
             Users
         </a>
         @endif
