f5e51a869a8d0688e74f88f4e80e05542608f295
Cyber_Cipher <janpauljabien@gmail.com>
2024-09-05 21:33:27 +0800

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,6 +26,26 @@
         @endforeach
     </ul>
     @else
-    <p>No notifications available.</p>
+        <div class="flex justify-center">
+            <div class="z-0 absolute m">
+                <img src="{{asset('assets/ISAT-U-logo.png') }}"  alt="" width="500" height="500">
+            </div>
+            <div class="z-10 p-2 ">
+                <div>
+                    <h1>
+                        Objectives
+                    </h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, error commodi ratione est aut aliquam veniam aspernatur, esse molestias optio quaerat dignissimos quas dolore laudantium id nesciunt, ipsa ad tenetur?</p>
+                </div>
+                <div>
+                    <h1>
+                        About
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam autem consectetur et iusto beatae magnam aliquam, tenetur sed debitis cupiditate, voluptatem nulla soluta enim eveniet corrupti consequuntur similique veniam perspiciatis.
+                    </p>
+                </div>
+            </div>
+        </div>
     @endif
 </x-layouts.app>
