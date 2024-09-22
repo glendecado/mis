1a1d7eab2874bc714e91a05bbc8402600ab242aa
glendecado <131495980+glendecado@users.noreply.github.com>
2024-09-22 13:27:44 +0000

    matched id for radio buttons

--- resources/views/livewire/request/view-request.blade.php
+++ resources/views/livewire/request/view-request.blade.php
@@ -38,22 +38,22 @@
 
             <div>
                 <input type="radio" id="2" wire:model.live="status" value="waiting" class="peer hidden" />
-                <label for="1" class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-900 peer-checked:font-bold peer-checked:text-white">waiting</label>
+                <label for="2" class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-900 peer-checked:font-bold peer-checked:text-white">waiting</label>
             </div>
 
             <div>
                 <input type="radio" id="3" wire:model.live="status" value="pending" class="peer hidden" />
-                <label for="2" class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-900 peer-checked:font-bold peer-checked:text-white">Pending</label>
+                <label for="3" class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-900 peer-checked:font-bold peer-checked:text-white">Pending</label>
             </div>
 
             <div>
                 <input type="radio" id="4" wire:model.live="status" value="ongoing" class="peer hidden" />
-                <label for="3" class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-900 peer-checked:font-bold peer-checked:text-white">Ongoing</label>
+                <label for="4" class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-900 peer-checked:font-bold peer-checked:text-white">Ongoing</label>
             </div>
 
             <div>
-                <input type="radio" id="4" wire:model.live="status" value="resolve" class="peer hidden" />
-                <label for="4" class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-900 peer-checked:font-bold peer-checked:text-white">Resolve</label>
+                <input type="radio" id="5" wire:model.live="status" value="resolve" class="peer hidden" />
+                <label for="5" class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-900 peer-checked:font-bold peer-checked:text-white">Resolve</label>
             </div>
 
         </div>
