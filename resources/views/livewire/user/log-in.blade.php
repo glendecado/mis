b6299a1dda426ae5b4484abbac4b8af1b68f1657
Glen Brian G. Decado <glen.decado@gmail.com>
2024-09-24 00:01:14 +0800

    label remains on top when input is not empty

--- resources/views/livewire/user/log-in.blade.php
+++ resources/views/livewire/user/log-in.blade.php
@@ -7,20 +7,37 @@
             </div>
             <div class="my-[50px]">
                 <form wire:submit.prevent="login" method="post">
+
                     <div class="relative mt-6">
-                        <input type="email" name="email" id="email" placeholder="Email Address" class="peer mt-1 w-full border-b-2 border-gray-300 px-1 py-3 placeholder:text-transparent focus:border-gray-500 focus:outline-none bg-azure rounded-md " autocomplete="NA" wire:model.blur="email" />
-                        <label for="email" class="pointer-events-none pl-3 absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-500 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-[-10px] peer-focus:left-2 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-white">Email Address</label>
+                        <input type="email" name="email" id="email" placeholder="Email Address"
+                            class="peer mt-1 w-full border-b-2 border-gray-300 px-5 py-3 placeholder:text-transparent focus:border-gray-500 focus:outline-none bg-azure rounded-md"
+                            wire:model.lazy="email" autocomplete="off" />
+
+                        <label for="email"
+                            class="pointer-events-none pl-3 absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-500 opacity-75 transition-all duration-100 ease-in-out {{ strlen($email) > 0 ? 'top-[-10px] left-2 pl-0 text-sm text-white' : 'peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-[-10px] peer-focus:left-2 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-white' }}">
+                            Email Address
+                        </label>
+
                         @if ($emailError)
                         <div class="mt-1 text-xs text-red-600 absolute">❌ {{ $emailError }}</div>
                         @endif
                     </div>
+
                     <div class="relative mt-9">
-                        <input type="password" name="password" id="password" placeholder="Password" class="peer mt-1 w-full border-b-2 border-gray-300 px-1 py-3 placeholder:text-transparent focus:border-gray-500 focus:outline-none bg-azure rounded-md" wire:model.blur="password" />
-                        <label for="password" class="pointer-events-none pl-3 absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-500 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-[-10px] peer-focus:left-2 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-white">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Password"
+                            class="peer mt-1 w-full border-b-2 border-gray-300 px-5 py-3 placeholder:text-transparent focus:border-gray-500 focus:outline-none bg-azure rounded-md"
+                            wire:model.lazy="password" autocomplete="off" />
+
+                        <label for="password"
+                            class="pointer-events-none pl-3 absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-500 opacity-75 transition-all duration-100 ease-in-out {{ strlen($password) > 0 ? 'top-[-10px] left-2 pl-0 text-sm text-white' : 'peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-[-10px] peer-focus:left-2 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-white' }}">
+                            Password
+                        </label>
+
                         @if ($passwordError)
                         <div class="mt-1 text-xs text-red-600 absolute">❌ {{ $passwordError }}</div>
                         @endif
                     </div>
+
 
                     <div class="my-[50px]">
                         <button type="submit" class="font-geist w-full rounded-md bg-blue-200 px-3 py-4 text-white focus:bg-gray-600 focus:outline-none hover:bg-yellow hover:text-black transition duration-300">Sign in</button>
