328a6785fed3d29e6671a3138d8e6015fec46e51
Glen Brian G. Decado <glen.decado@gmail.com>
2024-09-25 10:50:53 +0800

    when input task is empty

--- app/Livewire/TaskList/AddList.php
+++ app/Livewire/TaskList/AddList.php
@@ -13,12 +13,18 @@ class AddList extends Component
     #[On('add-task-list')]
     public function add($tasks, $id)
     {
- 
+        
+        if($tasks == null){
+            
+            $this->dispatch('error', name: 'Empty');
+
+        }else{
  
         TaskList::create(['category_id'=>$id, 'task' =>$tasks]);
         $this->dispatch('view-category');
         $this->dispatch('success', name: 'Task added successfully');
         $this->dispatch('input-category-reset');
+        }
     }
     public function render()
     {
