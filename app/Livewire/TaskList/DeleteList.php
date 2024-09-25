9da6ceb309df34a966ce4a54a974353126a1d6da
Glen Brian G. Decado <glen.decado@gmail.com>
2024-09-25 10:51:09 +0800

    add and delete task list

--- app/Livewire/TaskList/DeleteList.php
+++ app/Livewire/TaskList/DeleteList.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire\TaskList;
 
+use App\Models\TaskList;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class DeleteList extends Component
 {
+    #[On('delete-task-list')]
+    public function delete($id)
+    {
+        $tl = TaskList::find($id);
+
+
+        $tl->delete();
+        $this->dispatch('success', name: 'deleted successfully');
+
+
+        $this->dispatch('view-category');
+    }
     public function render()
     {
         return view('livewire.task-list.delete-list');
--- resources/views/livewire/category/view-category.blade.php
+++ resources/views/livewire/category/view-category.blade.php
@@ -1,5 +1,9 @@
 <div class="flex flex-row items-start gap-2 flex-wrap">
-    {{----}} <!-- Placeholder for potential future content or directives -->
+    {{--Livewire Component for Adding a deleting a Task List --}} <!-- Placeholder for potential future content or directives -->
+    @livewire('task-list.delete-list')
+
+    {{-- Livewire Component for Adding a Task List --}}
+    @livewire('task-list.add-list')
 
     @foreach ($categories as $category)
     <div class="w-[300px] m-10">
@@ -10,50 +14,76 @@
             {{$category->name}}
         </div>
 
-        <div>
+
+
+
+
+        <div class="bg-slate-600 h-[500px] overflow-auto">
+
+
             <!-- Task List for the Current Category -->
             @foreach ($category->TaskList as $t)
-            <div>
+            <div class="my-2 p-2">
+
+
                 {{-- Displaying individual task --}}
-                {{$t->task}}
+                @if($open == false)
+                {{$loop->index + 1}}: {{$t->task}}
+                @elseif($open && $openThis != $category->id )
+                {{$loop->index + 1}}: {{$t->task}}
+                @elseif($open && $openThis == $category->id )
+
+
+                <div class="flex justify-between">
+                    <div>
+                        {{$loop->index + 1}}: {{$t->task}}
+                    </div>
+                    <div>
+                        <button class="bg-red-900 text-white rounded-md p-1" @click="$dispatch('delete-task-list', {id: {{$t->id}} })">X</button>
+                    </div>
+
+                </div>
+
+                @endif
             </div>
             @endforeach
 
-            <!-- Conditional Rendering Based on 'open' State -->
-            @if ($open && $openThis == $category->id)
-            {{-- Livewire Component for Adding a Task List --}}
-            @livewire('task-list.add-list')
 
-            <div>
-                <div>
-                    {{-- Input for New Task --}}
-                    <input type="text" wire:model.live="tasks">
 
-                    {{-- Submit Button to Add Task --}}
-                    <button @click="$dispatch('add-task-list', {tasks: '{{$tasks}}', id: {{$category->id}} })">
-                        Submit
-                    </button>
 
-                    {{-- Cancel Button to Close Input --}}
-                    <button wire:click="submitInput([{{ $category->id }}, false])">
-                        x
-                    </button>
-                </div>
-            </div>
 
-            @elseif($open == false)
-            {{-- Button to Open Task Input --}}
-            <button class="w-full bg-blue-400" wire:click="submitInput([{{ $category->id }}, true])">
-                Add
-            </button>
-
-            @elseif($open && $openThis != $category->id)
-            {{-- Disabled Button for Other Categories when Input is Open --}}
-            <button class="w-full bg-blue-200" wire:click="submitInput([{{ $category->id }}, true])" disabled>
-                Add
-            </button>
-            @endif
         </div>
+        <!-- Conditional Rendering Based on 'open' State -->
+        @if ($open && $openThis == $category->id)
+
+
+        <div x-data="{ tasks: @entangle('tasks') }" class="flex justify-between gap-1">
+            {{-- Input for New Task --}}
+            <input type="text" x-model="tasks" Placeholder="add task....">
+
+            {{-- Submit Button to Add Task --}}
+            <button @click="$dispatch('add-task-list', { tasks: tasks, id: {{ $category->id }} })" class="bg-green-500 w-full p-1">
+                Submit
+            </button>
+
+            {{-- Cancel Button to Close Input --}}
+            <button wire:click="submitInput([{{ $category->id }}, false])" class="bg-red-500 px-3 rounded-sm w-full">
+                <-
+            </button>
+        </div>
+
+        @elseif($open == false)
+        {{-- Button to Open Task Input --}}
+        <button class="w-full bg-blue-400" wire:click="submitInput([{{ $category->id }}, true])">
+            Edit
+        </button>
+
+        @elseif($open && $openThis != $category->id)
+        {{-- Disabled Button for Other Categories when Input is Open --}}
+        <button class="w-full bg-blue-200" wire:click="submitInput([{{ $category->id }}, true])" disabled>
+            Edit
+        </button>
+        @endif
 
     </div>
     @endforeach
