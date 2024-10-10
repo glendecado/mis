b9c5d33679a3b1ff55531a61751baebace42943e
Glen Brian G. Decado <glen.decado@gmail.com>
2024-10-11 00:22:04 +0800

    notif icon

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -64,7 +64,11 @@
 
         <div x-cloak x-data="{open : false}">
             <div class="absolute right-3 h-[56px] flex justify-center items-center">
-                <button @click="open = !open" wire:click="isClicked">notif {{--change to icon--}}</button>
+                <button @click="open = !open" wire:click="isClicked">
+                    <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="25" height="25">
+                        <path d="M22.555,13.662l-1.9-6.836A9.321,9.321,0,0,0,2.576,7.3L1.105,13.915A5,5,0,0,0,5.986,20H7.1a5,5,0,0,0,9.8,0h.838a5,5,0,0,0,4.818-6.338ZM12,22a3,3,0,0,1-2.816-2h5.632A3,3,0,0,1,12,22Zm8.126-5.185A2.977,2.977,0,0,1,17.737,18H5.986a3,3,0,0,1-2.928-3.651l1.47-6.616a7.321,7.321,0,0,1,14.2-.372l1.9,6.836A2.977,2.977,0,0,1,20.126,16.815Z" />
+                    </svg>
+                </button>
 
 
                 <div class="bg-blue-50 h-[450px] w-[300px] absolute top-[45px] right-0 z-50 rounded-md p-2 overflow-scroll"
@@ -95,16 +99,17 @@
                     @endif
                     </p>
                 </div>
+                @php
+                $notifCount = Cache::get('notif-count-' . Auth::id());
+                @endphp
+                @if ($notifCount)
+                <h1 class="absolute right-[10px] top-2 text-sm text-white bg-red-500 w-5 h-5 flex justify-center" id="notif-count" style="border-radius: 100%;">
 
-                <h1 class="relative left-0 bottom-2 text-md text-red-900" id="notif-count">
-                    @php
-                    $notifCount = Cache::get('notif-count-' . Auth::id());
-                    @endphp
-                    @if ($notifCount)
+
                     {{$notifCount}}
-                    @endif
-                </h1>
 
+                </h1>
+                @endif
             </div>
         </div>
 
