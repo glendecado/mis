5c2ff4eab50d6d3c787ba155fc73f15d77cb0238
Glen Brian G. Decado <glen.decado@gmail.com>
2024-10-14 00:18:56 +0800

    cache on users

--- app/Livewire/Navbar.php
+++ app/Livewire/Navbar.php
@@ -33,13 +33,19 @@ class Navbar extends Component
     #[On('update-count')]
     public function render()
     {
-        $user = DB::table('users')
+        $user = Cache::rememberForever('users', function () {
+            return DB::table('users')
+            // Filter out users who have the role 'Mis Staff'
             ->where('role', '!=', 'Mis Staff')
+            // Apply additional search criteria using a nested function
+            //
             ->where(function ($query) {
+                // Search for users where the 'name', 'id', or 'email' matches the search term
                 $query->where('name', 'like', "%$this->search%")
-                    ->orWhere('id', 'like', "%$this->search%")
-                    ->orWhere('email', 'like', "%$this->search%");
+                ->orWhere('id', 'like', "%$this->search%")
+                ->orWhere('email', 'like', "%$this->search%");
             })->get();
+        });
         $request = Request::count();
         $route = $this->route;
         return view('livewire.navbar', compact('request', 'route', 'user'));
