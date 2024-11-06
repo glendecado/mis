c5cbc778c39ec2bafe4b6bef96d51573e502b83b
Glen Brian G. Decado <glen.decado@gmail.com>
2024-11-06 22:55:21 +0800

    check list update

--- resources/views/components/task-list/request-task-list.blade.php
+++ resources/views/components/task-list/request-task-list.blade.php
@@ -4,32 +4,47 @@ $i = 0;
 
 @foreach($this->viewTaskList() as $list)
 <div class="text-white">
-    <div 
-    class="bg-blue-300 rounded-md p-2 mb-2 cursor-pointer hover:bg-blue-200"                 
-    wire:click="check({{ $i < $checked ? $i : ($i == $checked ? $i + 1 : $i) }})">
-        <label class="cursor-pointer relative mr-2">
-            <input
-                type="checkbox"
-                value="{{ $i }}"
-                wire:click="check({{ $i < $checked ? $i : ($i == $checked ? $i + 1 : $i) }})"
-                {{ $i < $checked ? 'checked' : ($i == $checked ? '' : 'disabled') }}
-                class="{{ $i < $checked ? '' : ($i == $checked ? '' : 'bg-red-200') }} peer h-6 w-6 cursor-pointer transition-all appearance-none rounded-full bg-slate-100 shadow hover:shadow-md border border-slate-300 checked:bg-slate-800 checked:border-slate-800" id="check-custom-style" />
 
-            <span class="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-2 -translate-y-3">
+    {{--if check--}}
+        @if($i < $checked)
 
+        <div class="bg-blue-300 rounded-md p-2 mb-2 flex cursor-pointer" wire:click="check({{ $i }})">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 text-blue-500 mr-2">
+            <circle cx="12" cy="12" r="10" />
+                    <path d="M9 12l2 2 4-4" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+            </svg>
+            {{$list->task}}
+        </div>
 
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" stroke-width="1">
-                    <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+        {{--below checked--}}
+        @elseif($i == $checked)
+
+        <div class="group cursor-pointer" wire:click="check({{ $i + 1 }})">
+            <div class="bg-blue-300 rounded-md p-2 mb-2 flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" 
+                class="size-6 group-hover:text-blue-900 transition-colors duration-200">
+                    <circle cx="12" cy="12" r="10" />
+                    <path d="M9 12l2 2 4-4" class="group-hover:opacity-100 opacity-0 transition-opacity duration-200 bg-blue-300" fill="none" stroke="#60A5FA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
                 </svg>
+                <span class="ml-2 text-white">{{$list->task}}</span>
+            </div>
+        </div>
 
-
-            </span>
-
-        </label>
+        {{--disabled--}}
+        @else
+        <div class="bg-blue-300 rounded-md p-2 mb-2" >
         {{$list->task}}
-    </div>
+        </div>
+        @endif
+
 </div>
+
+{{--increment i every loop--}}
 @php
 $i++;
 @endphp
+
+
 @endforeach
