0e3792b38fed900d64294ac74ad14576cf246fb3
Glen Brian G. Decado <glen.decado@gmail.com>
2024-11-07 01:00:56 +0800

    middleware for request

--- app/Http/Middleware/RequestMiddleware.php
+++ app/Http/Middleware/RequestMiddleware.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Request as ModelsRequest;
+use App\Models\Task;
 use Closure;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Switch_;
 use Symfony\Component\HttpFoundation\Response;
 
 class RequestMiddleware
@@ -15,6 +18,50 @@ class RequestMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        //get id from /request/{id}
+        $id = $request->id;
+
+        $req = ModelsRequest::find($id);
+
+        $task = Task::where('request_id', $id)->get();
+
+        $tech = $task->pluck('technicalStaff_id')->toArray();
+
+
+        switch (session('user')['role']) {
+
+                //if mis - able to access all of the request
+            case 'Mis Staff':
+                return $next($request);
+                break;
+
+
+                //if faculty - Only the user’s own request can be accessed
+            case 'Faculty':
+                if ($req->faculty_id == session('user')['id']) {
+                    return $next($request);
+                } else {
+                    return redirect('/request');
+                }
+                break;
+
+
+                //if technical staff - check if tech id is in many task from one request;
+            case 'Technical Staff':
+
+                if (in_array(session('user')['id'], $tech)) {
+                    return $next($request);
+                } else {
+                    return redirect('/request');
+                }
+
+                break;
+
+
+            default:
+                return redirect('/request');
+                break;
+        }
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,16 @@ Route::middleware(['auth'])->group(function () {
 
   Volt::route('/dashboard', 'user/dashboard')->name('dashboard');
 
-  Volt::route('/request', 'request')->name('requests');
-  Volt::route('/request/{id}', 'request')->name('request');
 
   Volt::route('/profile/{id}', 'user/profile')->name('profile');
-  
+
   Volt::route('/admin-panel', 'admin/admin-panel')->name('admin-panel');
+  
+  Volt::route('/request', 'request')->name('requests');
+
+  Route::middleware(['request'])->group(function () {
+
+    Volt::route('/request/{id}', 'request')->name('request');
+  });
+
 });
