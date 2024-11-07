ff8e1c869817fa2158f1e5d79e470364528e15d6
Glen Brian G. Decado <glen.decado@gmail.com>
2024-11-07 23:56:56 +0800

    front end minor adjustment

--- resources/views/components/requests/add-request-button.blade.php
+++ resources/views/components/requests/add-request-button.blade.php
@@ -1,24 +1,23 @@
 <button
 
-x-data="{
+    x-data="{
 category : @entangle('category'),
 concern : @entangle('concerns')
 }"
 
- @click=" 
+    @click=" 
  $dispatch('open-modal', 'add-request-modal'); 
  category = 1;
  concern = '';
 
- "
- >
+ ">
     <div
         class="modal-trigger-button">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-            class=" size-12">
-            <path stroke-linecap="round" stroke-linejoin="round"
-                d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+</svg>
+
+
     </div>
 </button>
 
