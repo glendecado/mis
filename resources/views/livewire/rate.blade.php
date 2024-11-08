807ee42e2d1bc5a9809c7ad5a43f42ebf21a8a0d
Glen Brian G. Decado <glen.decado@gmail.com>
2024-11-08 14:44:10 +0800

    able to rate

--- resources/views/livewire/rate.blade.php
+++ resources/views/livewire/rate.blade.php
@@ -10,22 +10,41 @@ use function Livewire\Volt\{mount, state};
 //
 state(['rate', 'tasks']);
 
-mount(function(){
-    $this->tasks = Task::where('request_id', session('requestId'))->with('technicalStaff')->get();
+mount(function () {
+    $this->tasks = Task::where('request_id', session('requestId'))->with('technicalStaff')->with('rate')->get();
 });
 
-$addRate = function ($id, $rate) {
-    dd(['Task Id: '.$id,'Rate: '.$rate]);
+$addRate = function ($id, $FacultyRate) {
+
+
+    $rate = Rate::where('task_id', $id)->first();
+
+    if (is_null($rate)) {
+        $createRate = Rate::Create([
+            "task_id" => $id,
+            "rate" => $FacultyRate,
+        ]);
+        $this->dispatch('success', 'rated successfully');
+    } else {
+
+        $rate->update(["rate" => $FacultyRate]);
+        $rate->save();
+        $this->dispatch('success', 'rate updated');
+    }
 };
 
 $viewRate = function () {
-  
-/*    $this->technicalStaffId = $id;
+
+    /*    $this->technicalStaffId = $id;
     $this->averageRating = Rating::where('technicalStaff_id', $this->technicalStaffId)->avg('rating'); */
+    //$t = Task::where(technicalStaff_idm, $id)->with('rate')
+    //
+
+
 }
 ?>
 
 <div>
-   @include('components.rate.modal-rate')
+    @include('components.rate.modal-rate')
 
 </div>
