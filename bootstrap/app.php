353a617689f7375ccae9e5c3a7e991a9fa017cb8
Glen Brian G. Decado <glen.decado@gmail.com>
2024-11-12 08:34:20 +0800

    default image and trusted prxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+
+
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => RoleMiddleware::class,
             'request' => RequestMiddleware::class,
