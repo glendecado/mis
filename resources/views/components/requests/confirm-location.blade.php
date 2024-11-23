d134380eaee3467c23a3f0e39cc29f65137e0736
Glen Brian G. Decado <glen.decado@gmail.com>
2024-11-24 04:37:39 +0800

    modal

--- resources/views/components/requests/confirm-location.blade.php
+++ resources/views/components/requests/confirm-location.blade.php
@@ -10,39 +10,11 @@
             <div>College: <span x-text="college" class="font-bold"></span></div>
             <div>Building: <span x-text="building" class="font-bold"></span></div>
             <div>Room: <span x-text="room" class="font-bold"></span></div>
-            <button @click="$dispatch('open-modal','edit-loc')">Edit</button>
+            <button @click="$dispatch('open-modal','edit-loc')">
+                <x-icons.edit class="size-6"/>
+
+            </button>
         </fieldset>
     </div>
-
-    <x-modal name="edit-loc">
-    <div class="y mb-2" x-data="{
-        college: @entangle('college'),
-        building: @entangle('building'),
-        room: @entangle('room')
-    }">
-        <div class="md:x y gap-2 mb-2">
-            <div>
-                <label :class="{ 'text-gray-500': cl }" for="college" class="label">College</label>
-                <input x-model="college" type="text" class="input w-full uppercase" />
-            </div>
-            <div>
-                <label wire:model="building" :class="{ 'text-gray-500': cl }" for="building" class="label">Building</label>
-                <input x-model="building" type="text" class="input w-full uppercase" />
-            </div>
-            <div>
-                <label wire:model="room" :class="{ 'text-gray-500': cl }" for="room" class="label">Room</label>
-                <input x-model="room" type="text" class="input w-full uppercase" />
-            </div>
-        </div>
-
-        <!-- Disable button if any of the fields are empty -->
-        <button 
-            class="button" 
-            :disabled="!college || !building || !room" 
-            @click="$wire.confirmLocation(); $dispatch('close-modal', 'edit-loc')">
-            Submit
-        </button>
-    </div>
-</x-modal>
 
 </div>
