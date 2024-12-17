13694f4f82d032c4aae2a0a3d7230d890ab9eb7c
Glen Brian G. Decado <glen.decado@gmail.com>
2024-12-17 12:22:20 +0800

    adjust front end

--- resources/views/components/requests/faculty.blade.php
+++ resources/views/components/requests/faculty.blade.php
@@ -16,18 +16,18 @@
     <button class="button w-40" @click="$dispatch('open-modal', 'rateFeedback')">Rate & Feedback</button>
     <x-modal name="rateFeedback">
 
-        <div class="text-center">
-            <span class="text-lg font-bold">Provide feedback and ratings</span>
+        <div class="text-center mb-4">
+            <h1 class="text-[24px] font-bold">Provide feedback and ratings</h1>
         </div>
 
 
         <div>
-            <span>Rate our service</span>
+            <h2 class="font-bold text-lg">Rate our service</h2>
             <div x-data="{ rating: 0, feedback : '' }">
-                <div class="flex">
+                <div class="flex w-full justify-center gap-10 mt-4">
                     <!-- Loop through 5 stars and set the rating -->
                     <template x-for="star in [1, 2, 3, 4, 5]" :key="star">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer"
+                        <svg xmlns="http://www.w3.org/2000/svg" class="size-10 cursor-pointer text-center"
                             :class="{'text-yellow': star <= rating, 'text-gray-300': star > rating}"
                             fill="currentColor" viewBox="0 0 20 20" @click="rating = star">
                             <path d="M10 15.27l4.15 2.18a1 1 0 0 0 1.45-1.05l-.79-4.93 3.58-3.49a1 1 0 0 0-.55-1.71l-4.97-.72-2.22-4.5a1 1 0 0 0-1.8 0l-2.22 4.5-4.97.72a1 1 0 0 0-.55 1.71l3.58 3.49-.79 4.93a1 1 0 0 0 1.45 1.05L10 15.27z" />
@@ -36,12 +36,12 @@
                 </div>
 
 
-                <div class="y">
-                    <label>Share your feedback!</label>
-                    <textarea name="" id="" class="input" x-model="feedback"></textarea>
+                <div class="y mt-12">
+                    <label class="font-bold text-lg">Share your feedback!</label>
+                    <textarea name="feedback" id="feedback" class="input mt-4" x-model="feedback"></textarea>
                 </div>
 
-                <button class="button mt-4"
+                <button class="button float-end mt-4"
                     :disabled="rating === 0 || feedback === ''"
                     @click="$wire.feedbackAndRate(rating, feedback)">
                     Submit
