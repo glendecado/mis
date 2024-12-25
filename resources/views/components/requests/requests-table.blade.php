811b194bcca0cd32bbc778528c093d074318c5c1
Glen Brian G. Decado <glen.decado@gmail.com>
2024-12-26 00:25:45 +0800

    responsive

--- resources/views/components/requests/requests-table.blade.php
+++ resources/views/components/requests/requests-table.blade.php
@@ -1,23 +1,22 @@
 <!-- User Table for Larger Screens -->
-<div class="table-container md:block hidden w-full p-2 rounded-md" x-data="{ search: '' }">
+<div class="table-container w-full p-2 rounded-md" x-data="{ search: '' }">
     <!-- Search Input -->
     <div class="m-4">
-        <input 
-            type="text" 
-            placeholder="Search..." 
+        <input
+            type="text"
+            placeholder="Search..."
             x-model="search"
-            class="input w-96"
-        />
+            class="input w-96" />
     </div>
 
-    <table class="w-full text-[100%] break-words ">
-        <thead class="table-header">
+    <table class="w-full text-[100%] break-words " x-data="{ openRequest: false }">
+        <thead class="table-header hidden md:table-header-group ">
             <tr>
                 @if(session('user')['role'] != 'Faculty')
                 <th class="table-header-cell">Name</th>
                 @endif
                 <th class="table-header-cell">Date</th>
-                <th class="table-header-cell relative"> 
+                <th class="table-header-cell relative">
                     <div @click="status = !status" x-data="{ status: false }" class="flex flex-row items-center justify-center cursor-pointer flex-wrap">
                         <span>Status</span>
                         <!-- Arrow Icons -->
@@ -87,33 +86,31 @@
             </tr>
             @else
             @foreach ($this->viewRequest() as $request)
-            
+
             @php
             $color = "";
             switch($request->priorityLevel){
-                case 1:
-                    $color = "bg-red-500/50 hover:bg-red-500/30";
-                    break;
-                case 2:
-                    $color = "bg-yellow/50 hover:bg-yellow/30";
-                    break;
-                case 3:
-                    $color = "bg-green-500/50 hover:bg-green-500/30";
-                    break;
+            case 1:
+            $color = "bg-red-500/50 hover:bg-red-500/30";
+            break;
+            case 2:
+            $color = "bg-yellow/50 hover:bg-yellow/30";
+            break;
+            case 3:
+            $color = "bg-green-500/50 hover:bg-green-500/30";
+            break;
             }
-            
+
             @endphp
 
-            <tr 
-                class="{{session('user')['role'] == 'Technical Staff' ? $color : 'hover:bg-blue-100'}} table-row-cell  hover:border-y-blue-600 cursor-pointer" 
+            <tr
+                class="{{session('user')['role'] == 'Technical Staff' ? $color : 'hover:bg-blue-100'}} table-row-cell  hover:border-y-blue-600 cursor-pointer hidden md:table-row"
                 {{--Search--}}
                 x-show="search === '' || 
-                '{{ $request->faculty->user->name ?? '' }} {{ $request->status }} {{ $request->category->name }} {{ $request->concerns }} {{ $request->faculty->college}} {{ $request->faculty->building}} {{ $request->faculty->room}}'.toLowerCase().includes(search.toLowerCase())"
-            >
+                '{{ $request->faculty->user->name ?? '' }} {{ $request->status }} {{ $request->category->name }} {{ $request->concerns }} {{ $request->faculty->college}} {{ $request->faculty->building}} {{ $request->faculty->room}}'.toLowerCase().includes(search.toLowerCase())">
                 @if(session('user')['role'] != 'Faculty')
                 <td class="table-row-cell" @click="Livewire.navigate('/request/{{$request->id }}')">
                     {{ $request->faculty->user->name }}
-                    {{$request->priorityLevel}}
                 </td>
                 @endif
                 <td class="table-row-cell" @click="Livewire.navigate('/request/{{$request->id }}')">
@@ -142,58 +139,55 @@
                 </td>
                 @endif
             </tr>
+
+            <tr x-cloak class="bg-blue w-full md:hidden flex mb-2  rounded-md text-white overflow-hidden"
+                x-show="search === '' || 
+            '{{ $request->faculty->user->name ?? '' }} {{ $request->status }} {{ $request->category->name }} {{ $request->concerns }} {{ $request->faculty->college }} {{ $request->faculty->building }} {{ $request->faculty->room }}'.toLowerCase().includes(search.toLowerCase())">
+
+                <td class="flex flex-col m-2 w-full h-full" @click="openRequest = openRequest === '{{ $request->id }}' ? '' : '{{ $request->id }}'">
+                    <span class="font-bold mb-2">
+                        @if(session('user')['role'] == 'Faculty')
+                        {{ $request->created_at->format('Y-m-d') }}
+                        @else
+                        {{$request->faculty->user->name}}
+                        @endif
+                    </span>
+
+                    <span class="absolute right-3">
+                        <div x-show="openRequest != '{{$request->id}}' ">
+                            <x-icons.arrow direction="down" />
+                        </div>
+
+                        <div x-show="openRequest == '{{$request->id}}' ">
+                            <x-icons.arrow direction="up" />
+                        </div>
+                    </span>
+
+                    <span class="bg-white w-full h-full rounded-md text-black p-2 flex flex-col" x-show="openRequest == '{{$request->id}}' ">
+                        @if(session('user')['role'] != 'Faculty')
+                        <p>Date: {{$request->created_at->format('Y-m-d')}}</p>
+                        @endif
+                        <p>Status: {{ $request->status }}</p>
+                        <p>Category: {{$request->created_at->format('Y-m-d')}}</p>
+                        <p class="text-ellipsis overflow-hidden ">Concerns: {{$request->concerns}}</p>
+                        <p>Location: {{$request->created_at->format('Y-m-d')}}</p>
+                        <button class="button" @click="Livewire.navigate('/request/{{$request->id }}')">View</button>
+                        @if(session('user')['role'] == 'Faculty')
+
+                        <div @click="if (confirm('Are you sure you want to delete this request?')) $wire.deleteRequest({{ $request->id }})" class="absolute right-5">
+                            <x-icons.delete />
+                        </div>
+
+                        @endif
+                    </span>
+                </td>
+            </tr>
+
+
+
+
             @endforeach
             @endif
         </tbody>
-    </table>
-</div>
-
-<!-- User Table for Mobile Screens with Search -->
-<div class="table-container block md:hidden z-10 p-4" x-data="{ openRequest: '', search: '' }">
-    <!-- Search Input -->
-    <div class="m-4">
-        <input 
-            type="text" 
-            placeholder="Search..." 
-            x-model="search"
-            class="input"
-        />
-    </div>
-
-    <table class="min-w-full relative p-2">
-        @foreach ($this->viewRequest() as $request)
-        <tr class="table-row-cell"
-            x-show="search === '' || 
-            '{{ $request->faculty->user->name ?? '' }} {{ $request->status }} {{ $request->category->name }} {{ $request->concerns }}'
-                .toLowerCase().includes(search.toLowerCase())"
-            @click="openRequest = openRequest === '{{ $request->id }}' ? '' : '{{ $request->id }}'">
-            <td class="y border mb-2 bg-blue-100 rounded-md">
-                <span class="bg-blue rounded-md text-white">RequestId: {{ $request->id }}</span>
-                <span class="text-ellipsis overflow-hidden">Name: {{ $request->faculty->user->name }}</span>
-
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="size-6 absolute right-1 text-blue-50">
-                    <path x-show="openRequest != '{{$request->id}}' " stroke-linecap="round" stroke-linejoin="round"
-                        d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    <path x-show="openRequest =='{{$request->id}}'" stroke-linecap="round" stroke-linejoin="round"
-                        d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
-
-                <div class="y h-fit" x-show="openRequest === '{{ $request->id }}'" >
-                    <div>Status: <span>{{ $request->status }}</span></div>
-                    <div>Category: <span>{{ $request->category->name }}</span></div>
-                    <div>Concerns: <span>{{ $request->concerns }}</span></div>
-                    <div>Location: <span>{{ $request->faculty->college }} {{ $request->faculty->building }} {{ $request->faculty->room }}</span></div>
-                    @if(session('user')['role'] == 'Faculty')
-                    <button @click="if (confirm('Are you sure you want to delete this request?')) $wire.deleteRequest({{ $request->id }})">
-                        <x-icons.delete />
-                    </button>
-                    @endif
-                    <div @click="Livewire.navigate('/request/{{$request->id }}')" class="bg-yellow text-blue-2 h-7">view</div>
-                </div>
-
-            </td>
-        </tr>
-        @endforeach
     </table>
 </div>
