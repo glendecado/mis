8094496bf32cde8c62865647ba0dcbf0c9726134
Glen Brian G. Decado <glen.decado@gmail.com>
2025-01-10 01:16:36 +0800

    button disabled when loading

--- resources/views/components/assigned-request/modal.blade.php
+++ resources/views/components/assigned-request/modal.blade.php
@@ -13,13 +13,25 @@
                 <tr>
                     <td class="table-row-cell">{{ $tech->user->id }}</td>
                     <td class="table-row-cell">{{ $tech->user->name }}</td>
-                    <td class="table-row-cell"  wire:loading.attr="disabled">
+                    <td class="table-row-cell">
 
-                        @if(!in_array($tech->technicalStaff_id, $this->viewAssigned()))
-                        <button  class="button" wire:click.prevent="assignTask('{{$tech->user->id}}')">Assign</button>
-                        @else
-                        <button class="button" wire:click.prevent="removeTask('{{$tech->user->id}}')">Remove</button>
-                        @endif
+                        <div wire:loading.class="hidden">
+                            @if(!in_array($tech->technicalStaff_id, $this->viewAssigned()))
+                            <button class="button" wire:click.prevent="assignTask('{{$tech->user->id}}')">Assign</button>
+                            @else
+                            <button class="button" wire:click.prevent="removeTask('{{$tech->user->id}}')">Remove</button>
+                            @endif
+                        </div>
+
+                        <div wire:loading class="text-blue-900">
+                            @if(!in_array($tech->technicalStaff_id, $this->viewAssigned()))
+                            <button disabled class="button">Assign</button>
+                            @else
+                            <button disabled class="button">Remove</button>
+                            @endif
+                        </div>
+
+
                     </td>
                 </tr>
                 @endforeach
