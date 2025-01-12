0b4ad27a46f63bbf7ac04011de7c77d1e84fcd8a
Glen Brian G. Decado <glen.decado@gmail.com>
2025-01-12 10:29:56 +0800

    tool tip

--- resources/views/components/requests/add-request-button.blade.php
+++ resources/views/components/requests/add-request-button.blade.php
@@ -1,24 +1,31 @@
-<button
+<div class="group">
+    <button
 
-    x-data="{
+        x-data="{
 category : @entangle('category'),
 concern : @entangle('concerns')
 }"
 
-    @click=" 
+        @click=" 
  $dispatch('open-modal', 'add-request-modal'); 
  category = 1;
  concern = '';
 
  ">
-    <div
-        class="modal-trigger-button">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-</svg>
+        <div
+            class="modal-trigger-button">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
 
 
+        </div>
+    </button>
+
+    @include('components.requests.add-request-modal')
+
+
+    <div class="tooltip">
+        Add request
     </div>
-</button>
-
-@include('components.requests.add-request-modal')
+</div>
