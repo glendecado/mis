6bd489029676f642bf1f519d324090293dbf12b0
Glen Brian G. Decado <glen.decado@gmail.com>
2025-03-16 00:00:02 +0800

    front end update on assigned tech staff inside

--- resources/views/components/assigned-request/view-assigned-request.blade.php
+++ resources/views/components/assigned-request/view-assigned-request.blade.php
@@ -7,16 +7,20 @@
 <div class="flex flex-col y gap-2 bg-white text-black rounded-md mt-4">
     <span style="color: #2e5e91; font-size: 18px;">Assigned Technical Staff</span>
     @foreach($this->viewTechStaff($this->viewAssigned()) as $tech)
-    <div class="flex items-center gap-2 border p-2 mb-2 rounded-md relative" style="font-size: 16px;">
+    <div class="flex items-center gap-2 border p-2 mb-2 rounded-md relative flex-wrap" style="font-size: 16px;">
+
+        <div class="flex items-center gap-2">
+            <img src="{{asset('storage/'. $tech->user->img)}}" alt=""
+                class="rounded-full size-[3vw] md:size-16">
+
+            <span class="md:text-lg text-[3vw]">{{$tech->user->name}}</span>
+        </div>
 
 
-        <img src="{{asset('storage/'. $tech->user->img)}}" alt=""
-            class="rounded-full size-16">
-        {{$tech->user->name}}
 
 
         @if(session('user')['role'] == 'Mis Staff')
-        <button @click="$wire.removeTask({{$tech->user->id}})" wire:loading.attr="disabled" class="border h-full absolute right-0 flex items-center px-10 hover:bg-blue-100/50 cursor-pointer" wire:loading.class="bg-blue-100/50 cursor-progress">
+        <button @click="$wire.removeTask({{$tech->user->id}})" wire:loading.attr="disabled" class="border h-full absolute right-0 flex items-center px-10 hover:bg-blue-100/50 cursor-pointer bg-white" wire:loading.class="bg-blue-100/50 cursor-progress">
             <x-icons.delete />
         </button>
         @endif
