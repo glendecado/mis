e2b57d97fcff7d1bd592025dced23257563072e2
Glen Brian G. Decado <glen.decado@gmail.com>
2025-03-15 12:48:48 +0800

    fix errors

--- resources/views/components/category/manage-category.blade.php
+++ resources/views/components/category/manage-category.blade.php
@@ -1,54 +1,52 @@
-<div x-data="{ selectedCategoryId: null }" class="m-5 h-full p-4 table-container rounded-md"> <!-- Removed overflow-auto -->
-
-<div> <!-- Added padding on both sides -->
+<div class="m-5 h-full p-4 table-container rounded-md"><!-- Added padding on both sides -->
     <h1 class="text-3xl font-bold mt-4" style="color: #2e5e91;">Category</h1>
-</div>
 
-    <div class="p-4 transition-all space-y-3"> <!-- Improved spacing between categories -->
-        @foreach($this->viewCategory() as $category)
-        <div :class="selectedCategoryId === {{ $category->id }} ? 'rounded-lg border-r-2 border-b-2 border-l-2 h-fit border-blue-500 mt-2 rounded-b-md' : 'mt-2'">
-        <div
-    class="p-4 text-white cursor-pointer select-none flex justify-between items-center"
-    style="background-color: #2e5e91;"
-    :class="selectedCategoryId === {{ $category->id }} ? 'rounded-t-md' : 'rounded-md'"
-    @click="selectedCategoryId = selectedCategoryId === {{ $category->id }} ? null : {{ $category->id }}">
 
-    <!-- Delete button aligned with the category name -->
-    <div class="relative flex items-center justify-center bg-white rounded-full p-2">
-        <button wire:loading.attr="disabled" class="mr-4" @click="$wire.deleteCategory({{ $category->id }})">
-            <x-icons.delete />
-        </button>
-    </div>
+    <div x-data="{ selectedCategoryId: null }" class="m-3 overflow-auto h-[80vh] px-4"> <!-- Added padding here -->
 
-    <!-- Category Name -->
-    <span class="flex-grow text-center text-lg">{{$category->name}}</span>
+        <div class="p-4 transition-all space-y-3"> <!-- Improved spacing between categories -->
+            @foreach($this->viewCategory() as $category)
+            <div :class="selectedCategoryId === {{ $category->id }} ? 'rounded-lg border-r-2 border-b-2 border-l-2 h-fit border-blue-500 mt-2 rounded-b-md' : 'mt-2'">
+                <div
+                    class="p-4 text-white cursor-pointer select-none flex justify-between items-center"
+                    style="background-color: #2e5e91;"
+                    :class="selectedCategoryId === {{ $category->id }} ? 'rounded-t-md' : 'rounded-md'"
+                    @click="selectedCategoryId = selectedCategoryId === {{ $category->id }} ? null : {{ $category->id }}">
 
-    <!-- Toggle Arrow Icon -->
-    <span x-show="selectedCategoryId !== {{ $category->id }}">
-        <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
-            <path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z" />
-        </svg>
-    </span>
-    <span x-show="selectedCategoryId === {{ $category->id }}">
-        <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
-            <path d="M480-525 291-336l-51-51 240-240 240 240-51 51-189-189Z" />
-        </svg>
-    </span>
-</div>
+                    <div class="relative flex items-center justify-center bg-white rounded-full p-2">
+                        <button wire:loading.attr="disabled" class="mr-4" @click="$wire.deleteCategory({{ $category->id }})">
+                            <x-icons.delete />
+                        </button>
+                    </div>
+                    <span class="flex-grow text-center text-lg">{{$category->name}}</span>
+                    <span x-show="selectedCategoryId !== {{ $category->id }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
+                            <path d="M480-333 240-573l51-51 189 189 189-189 51 51-240 240Z" />
+                        </svg>
+                    </span>
+                    <span x-show="selectedCategoryId === {{ $category->id }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
+                            <path d="M480-525 291-336l-51-51 240-240 240 240-51 51-189-189Z" />
+                        </svg>
 
-                {{-- TaskList --}}
-                <livewire:task-list :category="$category->id" />
+                    </span>
+                </div>
+                <div x-show="selectedCategoryId === {{ $category->id }}" class="pt-4 p-2 relative">
+                    <p class="-translate-y-3 text-sm text-blue/80">"Drag and drop to rearrange the list items."</p>
+                    {{-- TaskList --}}
+                    <livewire:task-list :category="$category->id" />
 
+                </div>
+            </div>
+            @endforeach
+            <div x-data="{ input: '' }" class="mt-4">
+                <div class="relative w-full max-w-xs">
+                    <input type="text" x-model="input" class="input pr-20 p-3 w-full" style="height: 50px;" placeholder="Enter category...">
+
+                    <button @click="$wire.addCategory(input)" class="absolute right-0 top-0 bottom-0 px-4 text-white border-0 rounded-md m-2 z-10" style="background-color: #3E7B27;">
+                        Submit Category
+                    </button>
+                </div>
             </div>
         </div>
-        @endforeach
-        <div x-data="{ input: '' }" class="mt-4">
-            <div class="relative w-full max-w-xs">
-                <input type="text" x-model="input" class="input pr-20 p-3 w-full" style="height: 50px;" placeholder="Enter category...">
-                
-                <button @click="$wire.addCategory(input)" class="absolute right-0 top-0 bottom-0 px-4 text-white border-0 rounded-md m-2 z-10" style="background-color: #3E7B27;">
-                    Submit Category
-                </button>
-            </div>
     </div>
-</div>
--- resources/views/livewire/user/login.blade.php
+++ resources/views/livewire/user/login.blade.php
@@ -62,7 +62,7 @@ $login = function () {
 <div class="w-full flex items-center justify-center h-svh bg-blue-50">
     <div class="w-full flex justify-center m-4"> <!-- Changed justify-end to justify-center -->
         <div
-            class="relative w-full md:w-96 px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 rounded-md sm:px-10 h-[300px]" style="background-color: #2e5e91;">
+            class="relative w-full md:w-96 px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 rounded-md sm:px-10" style="background-color: #2e5e91;">
             <div class="flex items-center gap-2 justify-center mb-2">
                 <img src="{{ asset('storage/profile_images/default/ISAT-U-logo.png') }}" alt="ISAT-U Logo" class="size-10">
                 <p class="text-white font-medium text-xl font-geist">MIS Service Request Portal</p>
@@ -71,7 +71,7 @@ $login = function () {
             <hr>
 
             <div class="text-center">
-                <!-- <h1 class="font-geist text-3xl font-semibold text-blue-50">Sign in</h1> -->
+      
                 <p class="mt-4 text-blue-50">Sign in to access your account</p>
             </div>
             <div class="" style="margin-top: 40px;">
