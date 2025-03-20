bf680ea32cedaf892d2a90e4c2f81c2ffaff9464
Glen Brian G. Decado <glen.decado@gmail.com>
2025-03-20 15:50:33 +0800

    hide in table if already assigned

--- resources/views/components/assigned-request/modal.blade.php
+++ resources/views/components/assigned-request/modal.blade.php
@@ -1,39 +1,32 @@
 <x-modal name="assign-task-modal">
     <div class="table-container">
-    <table class="w-full">
-    <thead class="table-header" style="color: white; background-color: #2e5e91; font-size: 16px;">
-    <tr>
-        <th class="table-header-cell">Name</th>
-        <th class="table-header-cell">Actions</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($this->viewTechStaff() as $tech)
-    <tr>
-        <td class="table-row-cell" style="font-size: 16px;">{{ $tech->user->name }}</td>
-        <td class="table-row-cell">
-            <div wire:loading.class="hidden">
+        <table class="w-full">
+            <thead class="table-header" style="color: white; background-color: #2e5e91; font-size: 16px;">
+                <tr>
+                    <th class="table-header-cell">Name</th>
+                    <th class="table-header-cell">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($this->viewTechStaff() as $tech)
                 @if(!in_array($tech->technicalStaff_id, $this->viewAssigned()))
-                <button class="button" style="color: white; background-color: #2e5e91; font-size: 16px;"
-                    wire:click.prevent="assignTask('{{$tech->user->id}}')">Assign</button>
-                @else
-                <button class="button" style="color: white; background-color: #2e5e91; font-size: 16px;"
-                    wire:click.prevent="removeTask('{{$tech->user->id}}')">Remove</button>
-                @endif
-            </div>
+                <tr>
+                    <td class="table-row-cell" style="font-size: 16px;">{{ $tech->user->name }}</td>
+                    <td class="table-row-cell">
+                        <div wire:loading.attr="disabled">
 
-            <div wire:loading class="text-blue-900">
-                @if(!in_array($tech->technicalStaff_id, $this->viewAssigned()))
-                <button disabled class="button" style="color: white; background-color: #2e5e91; font-size: 16px;">Assign</button>
-                @else
-                <button disabled class="button" style="color: white; background-color: #2e5e91; font-size: 16px;">Remove</button>
+                            <button class="button" style="color: white; background-color: #2e5e91; font-size: 16px;"
+                                wire:click.prevent="assignTask('{{$tech->user->id}}')">Assign</button>
+
+
+                        </div>
+
+                    </td>
+                </tr>
                 @endif
-            </div>
-        </td>
-    </tr>
-    @endforeach
-    </tbody>
-    </table>
+                @endforeach
+            </tbody>
+        </table>
 
     </div>
 </x-modal>
