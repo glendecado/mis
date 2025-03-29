a4b9f84b1bce85b57809738fa09e55fbee696fe9
Glen Brian G. Decado <glen.decado@gmail.com>
2025-03-29 13:56:41 +0800

    removed wire:click.stop="$wire.toggleCheck({{ $list->id }}) because this function does not exist, add transistion-all for check box and i add the completed task/total task

--- resources/views/components/task-per-request/tech-staff.blade.php
+++ resources/views/components/task-per-request/tech-staff.blade.php
@@ -3,7 +3,7 @@
 <div class="flex flex-col">
     <div class="flex items-center justify-between">
         <p class="text-[16px]">Task To Do</p>
-        <span class="text-blue text-[16px] font-semibold mb-2">Completed Tasks: {{ $checked }}</span>
+        <span class="text-blue text-[16px] font-semibold mb-2">Completed Tasks: {{ $checked }} / {{$taskPerReq->count()}}</span>
     </div>
 
     @foreach ($taskPerReq as $list)
@@ -14,8 +14,7 @@
                 class="w-full flex items-center gap-2 p-2 rounded-md text-gray-800 text-sm font-thin transition bg-gray-200 hover:bg-gray-300">
                 
                 {{-- Checkbox Inside Button --}}
-                <div wire:click.stop="$wire.toggleCheck({{ $list->id }})"
-                    class="w-5 h-5 flex items-center justify-center border rounded-full transition 
+                <div class="w-5 h-5 flex items-center justify-center border rounded-full transition-all 
                            {{ $list->isCheck ? 'bg-blue-600 text-white border-blue-600' : 'border-blue-600 bg-white' }}">
                     @if ($list->isCheck)
                         <span class="text-[14px] font-bold">✓</span>
