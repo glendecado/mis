543fe3d146e3ddf459bc94bd626ffda25dd804a5
darlenejoy02 <darlenequebral2@gmail.com>
2025-04-01 14:15:12 +0000

    minot chnages again

--- resources/views/components/task-per-request/mis.blade.php
+++ resources/views/components/task-per-request/mis.blade.php
@@ -1,7 +1,7 @@
 @if (count($this->notDefault) > 0 && $this->taskList->isEmpty())
-<p class="text-red-500">No default task</p>
-<a x-navigate href="/category" class="underline text-sm"> Proceed to this link to add default task on a
-    category</a>
+<p class="p-2 border border-red-500 text-red-500 text-[16px] text-center rounded-md">ⓘ No default task.</p>
+<!-- <a x-navigate href="/category" class="underline text-sm"> Proceed to this link to add default task on a
+    category</a> -->
 @endif
 
 
@@ -27,26 +27,24 @@
 
 @endif
 
-
-
 @if ($this->notDefault)
-<div class="border p-2 rounded-md mt-5">
-    <p class="text-sm">there is a category that's not default,
-        <span class="underline text-red-500">
+<div class="border p-2 sm:p-4 rounded-md bg-gray-100 mt-5 w-full max-w-full">
+    <p class="text-sm sm:text-base">There is a category named
+        <span class="underline text-red-500 break-words">
             {{ implode(', ', $this->categories->whereNotNull('ifOthers')->pluck('ifOthers')->toArray()) }}
         </span>
+        that is not set as default.
     </p>
-    <p class="text-sm">Do you want to add it as a default?</p>
-    <div>
+    <p class="text-sm sm:text-base">Do you want to add it as a default category?</p>
+    <div class="mt-3 space-y-2 flex justify-end">
         @foreach ($this->categories->whereNotNull('ifOthers')->pluck('ifOthers')->toArray() as $name)
-        <div>
+        <div class="flex flex-wrap gap-2">
             <button @click="$wire.toDefaultCategory('{{ $name }}', true)"
-                class="border p-2 rounded-md bg-green-400 w-20 text-center">Yes</button>
+                class="border p-2 rounded-md bg-green-400 w-20 text-center text-white text-sm sm:text-base hover:bg-green-500 transition-colors">Yes</button>
             <button @click="$wire.toDefaultCategory('{{ $name }}', false)"
-                class="border p-2 rounded-md bg-red-400 w-20 text-center">No</button>
+                class="border p-2 rounded-md bg-red-400 w-20 text-center text-white text-sm sm:text-base hover:bg-red-500 transition-colors">No</button>
         </div>
         @endforeach
     </div>
-
 </div>
 @endif
