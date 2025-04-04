49ef86f5af9abad2c687d52a7b4ec9172da5efc7
darlenejoy02 <darlenequebral2@gmail.com>
2025-04-04 07:34:34 +0000

    fix header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,62 +1,52 @@
-<header class=" h-full p-2 flex items-center justify-between bg-[#578FCA]">
+<header class="h-full p-2 flex items-center justify-between bg-[#578FCA]">
 
     {{--Logo--}}
-    <div class="header-container-logo"
-        @click="Livewire.navigate('/request?status=all')">
+    <div class="header-container-logo" @click="Livewire.navigate('/request?status=all')">
         <x-logo />
         <span class="ml-2 md:block hidden">MIS Service Request Portal</span>
     </div>
 
-
-    {{--profile--}}
+    {{--Profile--}}
     <div class="x gap-2 items-center cursor-pointer select-none">
 
         <livewire:notif wire:key="notif" />
 
-        <div class="flex h-[50px] px-2 items-center gap-4 justify-between text-white border border-gray-100 rounded-md" x-data="{open : false}" @click="open = !open">
-            <div class="flex items-center gap-2 ">
+        <div class="flex h-[50px] px-2 items-center gap-4 justify-between text-white border border-gray-100 rounded-md" x-data="{ open: false }" @click="open = !open">
+            <div class="flex items-center gap-2">
 
                 <div class="flex flex-col justify-center w-full items-center">
-
                     <div class="md:block hidden w-32 text-center truncate text-sm">
                         {{ session('user')['email'] }}
                     </div>
 
                     <div class="md:block hidden text-xs">
-                        {{strtoupper(session('user')['role']) }}
+                        {{ strtoupper(session('user')['role']) }}
                     </div>
                 </div>
-                <img src="{{ asset('storage/' . session('user')['img']) }}" alt=""
-                class="rounded-full size-10 object-cover aspect-square">
 
+                <img src="{{ asset('storage/' . session('user')['img']) }}" alt=""
+                    class="rounded-full size-10 object-cover aspect-square">
             </div>
 
-            {{--arrow--}}
-            <div x-show="open ?? ''== false">
+            {{-- Arrow Down (default) --}}
+            <div x-show="!open">
                 <x-icons.arrow direction="down" />
             </div>
 
-            {{--open dropdown--}}
-            <div x-show="open ?? '' == true" x-cloak @click.outside="open = false">
-
+            {{-- Arrow Up (on click) --}}
+            <div x-show="open" x-cloak>
                 <x-icons.arrow direction="up" />
-
-                <div class="dropdown w-56 right-1 top-16 absolute md:right-7">
-
-                    <div class="dropdown-open-items p-4"
-                        @click="Livewire.navigate('/profile/{{session('user')['id']}}')">
-
-                        <x-icons.profile class="size-6 absolute left-5" />
-                        <span>Profile</span>
-                    </div>
-
-                    <livewire:user.logout />
-
-
-                </div>
             </div>
 
+            {{-- Dropdown (displayed when `open` is true) --}}
+            <div x-show="open" x-cloak @click.outside="open = false" class="dropdown w-56 right-1 top-16 absolute md:right-7">
+                <div class="dropdown-open-items p-4" @click="Livewire.navigate('/profile/{{ session('user')['id'] }}')">
+                    <x-icons.profile class="size-6 absolute left-5" />
+                    <span>Profile</span>
+                </div>
+
+                <livewire:user.logout />
+            </div>
         </div>
     </div>
-
 </header>
