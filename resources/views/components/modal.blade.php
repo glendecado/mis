c68c22d35b42257717a3449e202f4a8a368246dc
Glen Brian G. Decado <glen.decado@gmail.com>
2025-04-09 01:19:26 +0800

    remove x-show=""

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -49,10 +49,10 @@ $maxWidth = [
     x-on:keydown.escape.window="show = false; $dispatch('resetErrors')" 
     x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()" 
     x-on:keydown.shift.tab.prevent="prevFocusable().focus()" 
-    x-show="show ?? ''" 
+    x-show="show" 
 
     class="fixed inset-0 overflow-y-auto px-4 sm:px-0 z-40 w-full h-dvh">
-    <div x-show="show ?? ''" class="fixed inset-0 transform transition-all" x-on:click="show = false; $dispatch('resetErrors')" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
+    <div x-show="show" class="fixed inset-0 transform transition-all" x-on:click="show = false; $dispatch('resetErrors')" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
         <div class="absolute inset-0 bg-blue-2/90"></div>
     </div>
     <div class="w-[100%] h-[100vh] mt-[30px] ">
