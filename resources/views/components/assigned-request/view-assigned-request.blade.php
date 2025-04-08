771f0ded327166d91a547968e3ad438d23285bca
Glen Brian G. Decado <glen.decado@gmail.com>
2025-04-09 02:57:00 +0800

    view

--- resources/views/components/assigned-request/view-assigned-request.blade.php
+++ resources/views/components/assigned-request/view-assigned-request.blade.php
@@ -1,25 +1,7 @@
 @if(!empty($this->viewAssigned()))
-<div class="flex flex-col y gap-2 bg-white text-black rounded-md mt-4 relative">
+<div class="flex flex-col y gap-2 bg-white text-black rounded-md mt-4">
     <span class="text-[16px] text-[#2e5e91]">Assigned Technical Staff</span>
-    @foreach($this->viewTechStaff($this->viewAssigned()) as $tech)
-  
 
-        <span class="text-[12px]">{{$tech->user->name}}</span>
-
-
-
-
-
-        @if(session('user')['role'] == 'Mis Staff')
-         @if($ifPending)
-
-        <button @click="$wire.removeTask({{$tech->user->id}})" wire:loading.attr="disabled" class="border h-full absolute right-0 flex items-center px-10 hover:bg-blue-100/50 cursor-pointer bg-white" wire:loading.class="bg-blue-100/50 cursor-progress">
-            <x-icons.delete />
-        </button>
-        @endif
-        @endif
-
-    @endforeach
 </div>
 @endif
 
