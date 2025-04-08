ea8f6dc177010510877618e26521e90eafb26df8
Glen Brian G. Decado <glen.decado@gmail.com>
2025-04-08 10:05:56 +0800

    should queue email

--- app/Mail/CreatedAccount.php
+++ app/Mail/CreatedAccount.php
@@ -3,12 +3,13 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class CreatedAccount extends Mailable
+class CreatedAccount extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
